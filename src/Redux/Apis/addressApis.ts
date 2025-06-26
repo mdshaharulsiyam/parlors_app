7d79b69bccb800d7e9e3513b649089ba19ad725c
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-26 10:03:21 +0600

    address api created

--- src/Redux/Apis/addressApis.ts
+++ src/Redux/Apis/addressApis.ts
@@ -2,8 +2,45 @@ import { parlorsApi } from '../baseApis';
 
 const addressApis = parlorsApi.injectEndpoints({
   endpoints: (builder) => ({
-
+    getDivisions: builder.query({
+      query: () => ({
+        url: '/divisions/get-all',
+        method: 'GET'
+      }),
+    }),
+    getDistricts: builder.query({
+      query: ({ division_id }) => ({
+        url: '/districts/get-all',
+        method: 'GET',
+        params: {
+          division_id
+        }
+      }),
+    }),
+    getUpazilas: builder.query({
+      query: ({ district_id }) => ({
+        url: '/upazilas/get-all',
+        method: 'GET',
+        params: {
+          district_id
+        }
+      }),
+    }),
+    getUnions: builder.query({
+      query: ({ upazilla_id }) => ({
+        url: '/union/get-all',
+        method: 'GET',
+        params: {
+          upazilla_id
+        }
+      }),
+    })
   })
 })
 
-export const { } = addressApis
+export const {
+  useGetDivisionsQuery,
+  useGetDistrictsQuery,
+  useGetUpazilasQuery,
+  useGetUnionsQuery
+} = addressApis
