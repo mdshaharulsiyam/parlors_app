f29a95957de7cb78a893490edca44eb29133fbfc
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-30 14:02:07 +0600

    service add edit

---
+++ src/screens/stack/ServiceAddEdit.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ServiceAddEdit = () => {
+  return (
+    <View>
+      <Text>ServiceAddEdit</Text>
+    </View>
+  )
+}
+
+export default ServiceAddEdit
+
+const styles = StyleSheet.create({})
