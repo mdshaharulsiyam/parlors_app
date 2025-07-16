2c32200f5651bbfeef460a65040f4485d2fdb74e
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-07-16 16:40:03 +0600

    filter state

--- src/Redux/States/Filters.ts
+++ src/Redux/States/Filters.ts
@@ -1,11 +1,21 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface IFilters {
   search: string;
+  divisions: string;
+  districts: string;
+  upazilas: string;
+  unions: string;
+  street_address: string;
 }
 
 const initialState: IFilters = {
   search: '',
+  divisions: '',
+  districts: '',
+  upazilas: '',
+  unions: '',
+  street_address: '',
 };
 
 export const filtersSlice = createSlice({
@@ -15,8 +25,23 @@ export const filtersSlice = createSlice({
     setSearch: (state, action: PayloadAction<string>) => {
       state.search = action.payload;
     },
+    setDivisions: (state, action: PayloadAction<string>) => {
+      state.divisions = action.payload;
+    },
+    setDistricts: (state, action: PayloadAction<string>) => {
+      state.districts = action.payload;
+    },
+    setUpazilas: (state, action: PayloadAction<string>) => {
+      state.upazilas = action.payload;
+    },
+    setUnions: (state, action: PayloadAction<string>) => {
+      state.unions = action.payload;
+    },
+    setStreetAddress: (state, action: PayloadAction<string>) => {
+      state.street_address = action.payload;
+    },
   },
 });
 
-export const {setSearch} = filtersSlice.actions;
+export const { setSearch } = filtersSlice.actions;
 export default filtersSlice.reducer;
